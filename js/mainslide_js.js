cf145f55d72e0a712a5fc6323f1ebb559819bba0
jjest2222 <jjest2222@gmail.com>
2020-05-20 16:02:56 +0900

    js tab버튼 안어색하게 수정

--- js/mainslide_js.js
+++ js/mainslide_js.js
@@ -31,19 +31,22 @@ $(document).ready(function () {
     $(this).find(".slick-slide").not(".slick-active").find("a").hide();
     $(this).find(".slick-active a").show();
   });
+//---------------------------------------------------------------------------------------------------------------
+   // 예매권 등록
+   var flagTicket = false;
 
-  // 예매권 등록
-  var flagTicket = false;
-  var tab3click = function(){
-  $('.tab-3_btn').on('click', function () {
+   
 
-    if (flagTicket == false) {
-      $('.booking_dim').show();
-      $('.tab-3_btn').css('color', 'white');
-      $('.tab-3_btn').css('border-bottom', '2px solid #ddd');
-      $('.tab-1_btn').css('color', '#737373');
-      $('.tab-1_btn').css('border-bottom', 'none');
+   $('.tab-3_btn').on('click',function(){
+ 
+     if(flagTicket==false){
+       $('.booking_dim').show();
+       $('.tab-3_btn').css('color','white');
+       $('.tab-3_btn').css('border-bottom','2px solid #ddd');
+       $('.tab-1_btn').css('color','#737373');
+       $('.tab-1_btn').css('border-bottom','none');
 
+       //팝업 열린 상태에서 tab1 hover가능
       $('.tab-1_btn').on('mouseenter', function () {
         $('.tab-1_btn').css('color', 'white');
         $('.tab-1_btn').css('border-bottom', '2px solid #ddd');
@@ -52,57 +55,52 @@ $(document).ready(function () {
         $('.tab-1_btn').css('color', '#737373');
         $('.tab-1_btn').css('border-bottom', 'none');
       });
-      flagTicket = true;
-    } else {
-      $('.booking_dim').hide();
-      $('.tab-1_btn').css('color', 'white');
-      $('.tab-1_btn').css('border-bottom', '2px solid #ddd');
-      $('.tab-3_btn').css('color', '#737373');
-      $('.tab-3_btn').css('border-bottom', 'none');
 
+       flagTicket = true;
+     }
+     else{
+       $('.booking_dim').hide();
+       $('.tab-1_btn').css('color','white');
+       $('.tab-1_btn').css('border-bottom','2px solid #ddd');
+       $('.tab-3_btn').css('color','#737373');
+       $('.tab-3_btn').css('border-bottom','none');
+       flagTicket = false;
+     }
+   });
+ 
+   $('.bookingclose').on('click',function(){
+     $('.booking_dim').hide();
+     $('.tab-1_btn').css('color','white');
+     $('.tab-1_btn').css('border-bottom','2px solid #ddd');
+     $('.tab-3_btn').css('color','#737373');
+     $('.tab-3_btn').css('border-bottom','none');
+     flagTicket = false;
+   });
+ 
+   // js 작업 종료후, 마우스가 버튼 위로 올라갔을 경우 
+   $(".tab-3_btn").on("mouseenter", function() {
+     $('.tab-3_btn').css('color','white');
+     $('.tab-3_btn').css('border-bottom','2px solid #ddd');
 
-      $('.tab-3_btn').on('mouseenter', function () {
-        $('.tab-3_btn').css('color', 'white');
-        $('.tab-3_btn').css('border-bottom', '2px solid #ddd');
-      });
-    
-
-      $('.tab-1_btn').on('mouseenter', function () {
-        $('.tab-1_btn').css('color', 'white');
-        $('.tab-1_btn').css('border-bottom', '2px solid #ddd');
-      });
-      flagTicket = false;
-    }
-  });
-}
-tab3click();
-
-
-  $('.bookingclose').on('click', function () {
-    if (flagTicket == false) {
-    $('.booking_dim').hide();
-    $('.tab-1_btn').css('color', 'white');
-    $('.tab-1_btn').css('border-bottom', '2px solid #ddd');
-    $('.tab-3_btn').css('color', '#737373');
-    $('.tab-3_btn').css('border-bottom', 'none');
-}
-else{
-  $('.tab-3_btn').on('mouseenter', function () {
-    $('.tab-3_btn').css('color', 'white');
-    $('.tab-3_btn').css('border-bottom', '2px solid #ddd');
-  });
-  $('.tab-3_btn').on('mouseleave', function () {
-    $('.tab-3_btn').css('color', '#737373');
-    $('.tab-3_btn').css('border-bottom', 'none');
-  });
-
-  $('.tab-1_btn').on('mouseenter', function () {
-    $('.tab-1_btn').css('color', 'white');
-    $('.tab-1_btn').css('border-bottom', '2px solid #ddd');
-  });
-}
-  flagTicket = false;
-  });
+   });
+ 
+   // js 작업 종료후,  마우스가 버튼에서 나왔을 경우
+   $(".tab-3_btn").on("mouseleave", function() {
+     // 해당 메뉴를 클릭했으며, 버튼에서 마우스가 나왔을 경우
+     if (flagTicket == true) {
+         $('.tab-3_btn').css('color','white');
+         $('.tab-3_btn').css('border-bottom','2px solid #ddd');
+         $('.tab-1_btn').css('color','#737373');
+         $('.tab-1_btn').css('border-bottom','none');
+     }
+     else { // 해당 메뉴를 클릭하지 않고, 버튼에서 마우스가 나왔을 경우
+         $('.tab-1_btn').css('color','white');
+         $('.tab-1_btn').css('border-bottom','2px solid #ddd');
+         $('.tab-3_btn').css('color','#737373');
+         $('.tab-3_btn').css('border-bottom','none');
+     }
+   });
+ 
 
 
 });
