eb2e4e62728e8415e1deb627fc30c6f611c1115a
jjest2222 <jjest2222@gmail.com>
2020-05-28 15:50:34 +0900

    결제페이지 마무리

---
+++ js/bookingpage3.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    
+    
+
+
+var closepaykey = true;
+
+  $('.down').click(function () {
+
+      $('.article4').animate({
+        bottom: "-150"
+      }, 100);
+      closepaykey = false;
+ 
+  });
+  
+
+  
+});
+  
